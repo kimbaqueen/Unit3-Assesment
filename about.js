8e2d4bb93cbe89da24950c951511f6c645cdd287
Kim Starr <98854861+kimbaqueen@users.noreply.github.com>
2022-05-14 00:05:31 -0600

    completed

--- about.js
+++ about.js
@@ -4,11 +4,18 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+
+	// console.log('form submit');
+	alert("Form submitted!");
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const img = document.querySelector('#ducky');
+
+img.addEventListener('mouseover', () => {
+	alert('You are so good looking!');
+});
---
+++ profile.js
@@ -0,0 +1,17 @@
+const colorButton = document.querySelector('#color');
+
+colorButton.addEventListener('click', () => {
+    alert('Black and Yellow, Black and Yellow, Black and Yellow!');
+});
+
+const placeButton = document.querySelector('#place');
+
+placeButton.addEventListener('click', () => {
+    alert('Anywhere my family is <3');
+});
+
+const ritualButton = document.querySelector('#ritual');
+
+ritualButton.addEventListener('click', () => {
+    alert('Warm cup of coffee in the morning, and washing my face before bed.');
+});
